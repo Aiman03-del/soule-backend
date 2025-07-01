141823b245e28dd01a3a798281d19435aa001297
Aiman03-del <ausiaam83@gmail.com>
2025-07-01 12:01:38 +0600

    depoy frontend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ dotenv.config(); // ✅ .env config করার জন্য
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://soule-backend-h8dn.onrender.com'], 
+    origin: [
+      'http://localhost:5173',
+      'https://product-management-app-1.netlify.app',
+      'https://soule-backend-h8dn.onrender.com',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
